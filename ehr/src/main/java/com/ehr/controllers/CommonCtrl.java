3a73a82579fa5fc4a44c2a9961f075b40ae7fa2f
guobaoyu <guo_bao_yu@163.com>
2019-05-02 14:59:55 +0800

    通用模块-新增-CommonCtrl

---
+++ ehr/src/main/java/com/ehr/controllers/CommonCtrl.java
@@ -0,0 +1,48 @@
+package com.ehr.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ehr.common.service.QueryEmpService;
+import com.ehr.pojo.EhrEmployee;
+import com.ehr.pojo.ParamMapping;
+
+@Controller
+@RequestMapping("/common")
+public class CommonCtrl {
+	
+	@Autowired
+	QueryEmpService qeService;
+	
+	/**
+	 * @param ename
+	 * @return
+	 * 选择员工页-查询员工信息
+	 */
+	@RequestMapping("/queryEmpByName")
+	public ModelAndView queryEmpByName(String ename) {
+		ModelAndView mv = new ModelAndView();
+		try {
+			List<EhrEmployee> list = qeService.queryEmpByName(ename);
+			if(list==null || list.isEmpty()) {
+				mv.addObject("failed", ParamMapping.NO_RECORDS);
+			}else {
+				mv.addObject("employInfo",list);
+			}
+			mv.setViewName("select_employee");
+			return mv;
+		} catch (Exception e) {
+			e.printStackTrace();
+		    mv.setViewName("error");
+		    mv.addObject("failed", e.getMessage());
+		}
+		return mv;
+	}
+	
+	
+}
